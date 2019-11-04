9d262c7f117051c757ed940fc2c1b30e5fef764b
Mihaela Mihova <mmihova@codixfr.private>
2019-11-04 11:20:36 +0200

    initial front end commit

---
+++ MovieProject/form.js
@@ -0,0 +1,31 @@
+
+// function test(){
+//     alert("Mihaela");
+// }
+
+//$('#login-button').click(function
+
+function showLoginForm(){
+    $('#login-button').fadeOut("slow",function(){
+      $("#container").fadeIn();
+      TweenMax.from("#container", .4, { scale: 0, ease:Sine.easeInOut});
+      TweenMax.to("#container", .4, { scale: 1, ease:Sine.easeInOut});
+    });
+  };
+  //$(".close-btn").click(function
+  function closeBtn(){
+    TweenMax.from("#container", .4, { scale: 1, ease:Sine.easeInOut});
+    TweenMax.to("#container", .4, { left:"0px", scale: 0, ease:Sine.easeInOut});
+    $("#container, #forgotten-container").fadeOut(800, function(){
+      $("#login-button").fadeIn(800);
+    });
+  };
+  
+  /* Forgotten Password */
+  //$('#forgotten').click(function()
+  function forgottenPass(){
+    $("#container").fadeOut(function(){
+      $("#forgotten-container").fadeIn();
+    });
+  };
+  
